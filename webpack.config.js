7d4a44bd905b0efe60c1ca14a2d2f518a3a9d496
ApurvR20 <62154933+ApurvR20@users.noreply.github.com>
2024-05-09 14:55:33 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode : 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+
+        test: /\.png/,
+ 
+        type: 'asset/resource'
+ 
+      }
+    ]
+  },
+};
